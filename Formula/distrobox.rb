03bae7eb0eb4d0871f4bdd742b4b93acf738f6f4
aytea14 <amartukiminj@gmail.com>
2025-09-12 22:23:24 +0800

    feat(tap): add Distrobox formula for Linux distributions
    Closes #1.
    
    Signed-off-by: Amar Tukimin <amartukiminj@gmail.com>

---
+++ Formula/distrobox.rb
@@ -0,0 +1,26 @@
+class Distrobox < Formula
+  desc "Use any Linux distribution inside your terminal"
+  homepage "https://distrobox.it"
+  url "https://github.com/89luca89/distrobox/archive/refs/tags/1.8.1.2.tar.gz"
+  sha256 "3ecbce9b8c5b5df941f986798ffa6ea7fdf742223d42204207974c4323d5b9fc"
+  license "GPL-3.0-only"
+  head "https://github.com/89luca89/distrobox.git", branch: "main"
+
+  depends_on :linux
+
+  def install
+    system "./install", "--prefix", prefix
+  end
+
+  def caveats
+    <<~EOS
+      Distrobox requires one of podman or docker. Do
+        brew install podman
+      or consult the documentation for details.
+    EOS
+  end
+
+  test do
+    system bin/"distrobox-create", "--dry-run"
+  end
+end
