4f39c245e94b5a8c709a816c7395d3fe55b8a73f
Amar Tukimin <amartukiminj@gmail.com>
2025-09-12 20:54:49 +0000

    qwen-code: add 0.0.11 bottle.

--- Formula/qwen-code.rb
+++ Formula/qwen-code.rb
@@ -5,6 +5,11 @@ class QwenCode < Formula
   sha256 "88a561e5882793c52ec6e6946eb736b418501cacbeceb47120ce25f87db22894"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://ghcr.io/v2/amrkmn/tap"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "bc73ae41351c0334028341e9ac3906dc730bc418313c9489f791cd509792bc07"
+  end
+
   depends_on :linux
   depends_on "node"
 
