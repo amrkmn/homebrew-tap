cf0cf0ca704eb65168020097cc6b36de878b4769
Amar Tukimin <amartukiminj@gmail.com>
2025-09-16 11:44:49 +0000

    opencode: add 0.9.3 bottle.

--- Formula/opencode.rb
+++ Formula/opencode.rb
@@ -5,6 +5,11 @@ class Opencode < Formula
   sha256 "9e6bc174765d084898c64be9297e3705be141a2d3959f238368ef6379088e10b"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/amrkmn/tap"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "4cec0b07411ee2f5c1c683fc3149183ccf98a266f1800bc691a9929752b6b883"
+  end
+
   depends_on :linux
   depends_on "node"
 
