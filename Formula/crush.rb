fe6986dccaa6e7a1fea58f9967b25fae0d1fdb72
Amar Tukimin <amartukiminj@gmail.com>
2025-09-16 12:15:35 +0000

    crush: add 0.8.2 bottle.

--- Formula/crush.rb
+++ Formula/crush.rb
@@ -5,6 +5,11 @@ class Crush < Formula
   sha256 "723440e58ed2ebeeb47eee8e31f490b23fb553e32d98164ec182dc12de77e7e8"
   license "FSL-1.1-MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/amrkmn/tap"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "4ab7b146252e6438a4d4c0ecefff8c35f2deec6c71c3e5a97dad6054faba81b2"
+  end
+
   depends_on :linux
   depends_on "node"
 
