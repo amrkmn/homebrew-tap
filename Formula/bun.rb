c1e2b30396d7b4200052ed8ac3fcf17b152bc8cc
aytea14 <amartukiminj@gmail.com>
2025-09-17 22:04:44 +0800

    bun 1.2.22

---
+++ Formula/bun.rb
@@ -0,0 +1,39 @@
+class Bun < Formula
+  desc "Blazing fast JS runtime, bundler, tester & package manager in one"
+  homepage "https://bun.sh/"
+  url "https://registry.npmjs.org/bun/-/bun-1.2.22.tgz"
+  sha256 "4fa9880e1d552af2348d9b35024a0627f6ef4289bd272dd7dcfe4cbe1b010785"
+  license "MIT"
+
+  depends_on :linux
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+    generate_completions_from_executable(bin/"bun", "completions")
+  end
+
+  def post_install
+    avx2 = Hardware::CPU.features.include?(:avx2)
+    is_musl = File.exist?("/etc/alpine-release")
+
+    keep = if avx2 && is_musl
+      "bun-linux-x64-musl"
+    elsif avx2 && !is_musl
+      "bun-linux-x64"
+    elsif !avx2 && is_musl
+      "bun-linux-x64-musl-baseline"
+    else
+      "bun-linux-x64-baseline"
+    end
+
+    Dir["#{libexec}/lib/node_modules/bun/node_modules/@oven/*"].each do |dir|
+      FileUtils.rm_rf(dir) unless dir.end_with?(keep)
+    end
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/bun --version")
+  end
+end
