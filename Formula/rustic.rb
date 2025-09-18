13a29cfc07ee6a3f251d07f99383ecc8122486f3
Amar Tukimin <amartukiminj@gmail.com>
2025-09-18 09:32:23 +0000

    rustic: add 0.10.0 bottle.

--- Formula/rustic.rb
+++ Formula/rustic.rb
@@ -5,6 +5,13 @@ class Rustic < Formula
   sha256 "d67cc86a0e589788c74bee8ab86fd9637b64253751c03fb35a16455457a4684b"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://ghcr.io/v2/amrkmn/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "410eb121e3b83c2c1bc73403bf8900da0588159d2b96b3ee60bf2f1546f7b66f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1d8a5509f1b90518a86e975e34b79370677cea6909a2b9bf7c2b857d01f30cbb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b15d9813f8dd68d5e3cdfb5e05f4d244becbd6f138cbd016267f39945b31eb27"
+  end
+
   depends_on "rust" => :build
 
   def install
