2bdc085548ef10652241f4ab6f41ab7548225abe
Amar Tukimin <amartukiminj@gmail.com>
2025-09-19 16:25:47 +0000

    qwen-code: update 0.0.12 bottle.

--- Formula/qwen-code.rb
+++ Formula/qwen-code.rb
@@ -12,7 +12,10 @@ class QwenCode < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/amrkmn/tap"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "bc73ae41351c0334028341e9ac3906dc730bc418313c9489f791cd509792bc07"
+    rebuild 1
+    sha256                               arm64_tahoe:   "3955c6c045810a8e08aa8c6f8b0cca1af10e1f694e3acfd4c4ef56ee019815af"
+    sha256                               arm64_sequoia: "20988a0dc354e3ec90b6fe89b7e71d4893d609b7174da1e161d1b7d7ad75659e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "66bc13f90fffc7561925a89d5de13e3e7c527a08142f56485e0a94c8663bb24a"
   end
 
   depends_on "node"
