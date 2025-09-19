ad1e478ebb4b927bed378792e604bd42545bdc22
Amar Tukimin <amartukiminj@gmail.com>
2025-09-19 02:21:44 +0000

    crush: update 0.9.1 bottle.

--- Formula/crush.rb
+++ Formula/crush.rb
@@ -7,7 +7,9 @@ class Crush < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/amrkmn/tap"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "3f34c55912fbf07c19db2b53c7ef9ec129b0d7ddaae19e368d11a72bd5960b65"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "69c1a8b64c6deee4445d8892208d8472fe35d95260918e2c9f0c58c0509d3abd"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "254bc389b4bcb9aa60ef590d912b3f1c75334eefa773a56b67bb72e7680203b0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7b75651542922dcc75401d8ab8c2db1b00b0b47a093139a74685469f090b5400"
   end
 
   depends_on "node"
