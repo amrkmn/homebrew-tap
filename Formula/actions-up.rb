7e8e9d59156ed572e6f02da613de7fba0163a8b2
Amar Tukimin <amartukiminj@gmail.com>
2025-09-20 09:59:00 +0000

    actions-up: update 1.4.1 bottle.

--- Formula/actions-up.rb
+++ Formula/actions-up.rb
@@ -7,7 +7,9 @@ class ActionsUp < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/amrkmn/tap"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "21061ccf6a07874651a6339583e5540e2a4cb1e9b26ea45c6f38e3a5f5d4bfbb"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "29af32673db20e6e9adf8f21c04fca1962dd732ca1402305b27b07735010b88a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e19adf8452cc27b5af374606d54f3232919b330a4296d7e86353af8fd16321f9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e1e9d643bcc222c39043d6d6517e4361da41ba59d4d78a27b2b20239776b2556"
   end
 
   depends_on "node"
