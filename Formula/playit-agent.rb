ed0b16d907a461b3052454a42b5f93d2dfaccf43
aytea14 <amartukiminj@gmail.com>
2025-09-22 23:45:11 +0800

    chore(playit-agent): remove unnecessary dependencies
    Closes #51.
    
    Signed-off-by: Amar Tukimin <amartukiminj@gmail.com>

--- Formula/playit-agent.rb
+++ Formula/playit-agent.rb
@@ -6,9 +6,7 @@ class PlayitAgent < Formula
   license "BSD-2-Clause"
   head "https://github.com/playit-cloud/playit-agent.git", branch: "master"
 
-  depends_on "pkgconf" => :build
   depends_on "rust" => :build
-  depends_on "openssl@3"
 
   def install
     system "cargo", "install", *std_cargo_args(path: "packages/agent_cli")
