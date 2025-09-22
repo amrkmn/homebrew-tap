83c12aaf2a1b9bd123bffd9b364a5d8a50d2d717
aytea14 <amartukiminj@gmail.com>
2025-09-22 23:38:45 +0800

    playit-agent 0.16.3

---
+++ Formula/playit-agent.rb
@@ -0,0 +1,22 @@
+class PlayitAgent < Formula
+  desc "Secure tunnel client to expose local servers via Playit.gg"
+  homepage "https://github.com/playit-cloud/playit-agent"
+  url "https://github.com/playit-cloud/playit-agent/archive/refs/tags/v0.16.3.tar.gz"
+  sha256 "c0c5b9c5b1390a3966badd5d667f66aa5dbf88d360f8b51250ae29ea18f98479"
+  license "BSD-2-Clause"
+  head "https://github.com/playit-cloud/playit-agent.git", branch: "master"
+
+  depends_on "pkgconf" => :build
+  depends_on "rust" => :build
+  depends_on "openssl@3"
+
+  def install
+    system "cargo", "install", *std_cargo_args(path: "packages/agent_cli")
+
+    bin.install_symlink "playit-cli" => "playit"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/playit version")
+  end
+end
