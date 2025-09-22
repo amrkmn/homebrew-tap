ccb9fd031225d017a6d2f98601fdd32c8f4f6181
Amar Tukimin <amartukiminj@gmail.com>
2025-09-22 15:55:06 +0000

    playit-agent: add 0.16.3 bottle.

--- Formula/playit-agent.rb
+++ Formula/playit-agent.rb
@@ -6,6 +6,13 @@ class PlayitAgent < Formula
   license "BSD-2-Clause"
   head "https://github.com/playit-cloud/playit-agent.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/amrkmn/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "cf38660fc157db0caf428656b5f7b6d8ee0f39c7b5b2d2e029972bc9a331c7f9"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bdbf588dfddcaad1659272ee98fc58dacbde387a164519bd1da08b173e3d179c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5cde4f05f2c15d2479f1f2d6fd96d7b6504bb4907440e2a0ac31c00df02e91b9"
+  end
+
   depends_on "rust" => :build
 
   def install
