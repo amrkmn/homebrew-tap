fa95b201aed0cb24d8f232635e57c02a5cfbd4a9
aytea14 <amartukiminj@gmail.com>
2025-09-22 21:17:07 +0800

    croc 10.2.4

---
+++ Formula/croc.rb
@@ -0,0 +1,32 @@
+class Croc < Formula
+  desc "Easily and securely send things from one computer to another :crocodile: :package:"
+  homepage "https://schollz.com/software/croc6"
+  url "https://github.com/schollz/croc/archive/refs/tags/v10.2.4.tar.gz"
+  sha256 "c259c07b9da3ea39726b0c5e3f78ae66e858e1379bdb11bef93d31298e68f5fe"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    # As of https://github.com/schollz/croc/pull/701 an alternate method is used to provide the secret code
+    ENV["CROC_SECRET"] = "homebrew-test"
+
+    port=free_port
+
+    fork do
+      exec bin/"croc", "relay", "--ports=#{port}"
+    end
+    sleep 3
+
+    fork do
+      exec bin/"croc", "--relay=localhost:#{port}", "send", "--code=homebrew-test", "--text=mytext"
+    end
+    sleep 3
+
+    assert_match shell_output("#{bin}/croc --relay=localhost:#{port} --overwrite --yes homebrew-test").chomp, "mytext"
+  end
+end
