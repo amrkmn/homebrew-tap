62b54b0474c92d4d76622c1a8a54096f9cd708c5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-23 21:52:39 +0000

    crush 0.12.2
    Closes #114.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/crush.rb
+++ Formula/crush.rb
@@ -1,8 +1,8 @@
 class Crush < Formula
   desc "Glamourous AI coding agent for your favourite terminal"
   homepage "https://charm.sh/crush"
-  url "https://registry.npmjs.org/@charmland/crush/-/crush-0.12.1.tgz"
-  sha256 "a9d3d3098d929386e3e75adb2b66785c8660e171c06bf18ff8c8217ec4db50eb"
+  url "https://registry.npmjs.org/@charmland/crush/-/crush-0.12.2.tgz"
+  sha256 "775118d438c13182ffea1ecf5c6a6aa121a65614a53056c45e871ee95a9c0aa5"
   license "FSL-1.1-MIT"
 
   bottle do
