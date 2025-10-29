d5c820a16b9323a52ace044e7531b4c6cab630d8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-29 13:31:41 +0000

    qwen-code: update 0.1.1 bottle.

--- Formula/qwen-code.rb
+++ Formula/qwen-code.rb
@@ -7,9 +7,9 @@ class QwenCode < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/amrkmn/tap"
-    sha256                               arm64_tahoe:   "f2e0d278babe080f803ec178373791ef12300ff6eee0a9c53c6b913c31275729"
-    sha256                               arm64_sequoia: "01cfe666a82c20ff5f3c882f213f7582c04cb884db8880cdb00aa9797a33d665"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "97ce68db2f9cef521f21ac8b027a18857eed8dacc2a981f7ef67b74067b5c1c5"
+    sha256                               arm64_tahoe:   "441a38bdbafc672853f7238f09ecc03fb149ec8e4073c733bb68ead0ab05b435"
+    sha256                               arm64_sequoia: "1248eaab2c7748d572df5eda79ed001662a2b4d029245b716a7fe85c382a45e6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "62746bed7bb071d4050f4984cab537c3a2d4006774c60e54cd312dfd1b91db8f"
   end
 
   depends_on "node"
