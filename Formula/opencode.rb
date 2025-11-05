9f88f549886d90c09282e5f4a1a1d23cdafb813c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-05 02:31:30 +0000

    opencode: update 1.0.24 bottle.

--- Formula/opencode.rb
+++ Formula/opencode.rb
@@ -7,9 +7,9 @@ class Opencode < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/amrkmn/tap"
-    sha256                               arm64_tahoe:   "7afcd556ab7bcf1e99ca792d491a1d802e2526a0763544d15d16032538219e7a"
-    sha256                               arm64_sequoia: "214841f856d31fccb7e757594d658cca98162963907199644b1fda04fbfef1e0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "86bcd8499c1ac9044fe9b772145ab33fb258fea1cb5ae1834cd5482067aa0496"
+    sha256                               arm64_tahoe:   "d621e37916cad554dac7e1db7c4b30a8090f5190a0b580ea1d719ff9a48aa1ff"
+    sha256                               arm64_sequoia: "2768cf698419af75e251b875456fa2e439bcf280d1640fcc8480ebf0a3d73020"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d7930631f4e9c59963feb1fc598acf0e917459022ef9dd05475a795803ac6bf4"
   end
 
   depends_on "node"
