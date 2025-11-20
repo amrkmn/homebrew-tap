b5ea9f71fdb6218667a0978eac79787c5a56356f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-20 02:26:37 +0000

    croc: update 10.3.0 bottle.

--- Formula/croc.rb
+++ Formula/croc.rb
@@ -8,9 +8,9 @@ class Croc < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/amrkmn/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "83c39291ac516b641010f890dfd41c0d699a0c7bd7138f0b2856d4209f7005a4"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "41d4d16289ec89d1b64d5cb6e49f7d8f00b912a3ec4e7dac89fa142ddc05a5e0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7e5e34b5fd8c2ef7a63a96c979b1e661f14a3159199dba80b5e6c5bac49037e5"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4a9447ed6a20b12009dce0f53ec06d799ac7c341c7927d624157659bf0a686c3"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ec0fc953044bd7cff1b23852c645fb3ec4007d679b921f2e59e965ad8d25ec8f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1e6670d4e31056057c49bbeed0ac6bf0b87d72450e31b36391635528750bedcb"
   end
 
   depends_on "go" => :build
