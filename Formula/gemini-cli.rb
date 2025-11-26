471ff8dd5564c06011f696559e929223aa79b2f7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-26 04:03:21 +0000

    gemini-cli 0.18.0
    Closes #256.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/gemini-cli.rb
+++ Formula/gemini-cli.rb
@@ -1,8 +1,8 @@
 class GeminiCli < Formula
   desc "Interact with Google Gemini AI models from the command-line"
   homepage "https://github.com/google-gemini/gemini-cli"
-  url "https://registry.npmjs.org/@google/gemini-cli/-/gemini-cli-0.17.1.tgz"
-  sha256 "6e506cba746f3f24ef9ed0d8847e07003f34852de759af1923191e0d2bc2d95b"
+  url "https://registry.npmjs.org/@google/gemini-cli/-/gemini-cli-0.18.0.tgz"
+  sha256 "0e24ac7424304a32a473ea05e2d7e7d49faf97d21e4c08d17842dbfca174ca92"
   license "Apache-2.0"
 
   livecheck do
