356dd00bb026c62c072bbcf997bc221ba5cd652d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-05 07:06:40 +0000

    gemini-cli: update 0.19.2 bottle.

--- Formula/gemini-cli.rb
+++ Formula/gemini-cli.rb
@@ -14,7 +14,7 @@ class GeminiCli < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/amrkmn/tap"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "7b6b53142e6c3f6c4ee6ddd660ce6c47f3c789dce804c14f613b624fa357893a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "b85adf7db314ec6031d470b953747d40164cb6cf635543cf142a4cb42bb70f33"
   end
 
   depends_on "node"
