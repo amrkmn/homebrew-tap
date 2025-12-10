a3699b2cb18d97c94110136c04175546afc5b3be
amrkmn <amartukiminj@gmail.com>
2025-12-10 20:56:57 +0800

    fix(gemini-cli): remove unnecessary auth method check

--- Formula/gemini-cli.rb
+++ Formula/gemini-cli.rb
@@ -42,6 +42,5 @@ class GeminiCli < Formula
 
   test do
     assert_match version.to_s, shell_output("#{bin}/gemini --version")
-    assert_match "Please set an Auth method", shell_output("#{bin}/gemini --prompt Homebrew 2>&1", 1)
   end
 end
