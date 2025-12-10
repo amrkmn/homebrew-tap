fe5ee21abbeb92e3b79e95c866b79527e49cc9a9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-10 15:56:10 +0000

    crush 0.22.2

--- Formula/crush.rb
+++ Formula/crush.rb
@@ -1,8 +1,8 @@
 class Crush < Formula
   desc "Glamourous AI coding agent for your favourite terminal"
   homepage "https://charm.sh/crush"
-  url "https://registry.npmjs.org/@charmland/crush/-/crush-0.22.1.tgz"
-  sha256 "53a5027d34a546b48d0e5c8de2dfa85609c26a717d64b2dc78846a9687ab1db4"
+  url "https://registry.npmjs.org/@charmland/crush/-/crush-0.22.2.tgz"
+  sha256 "836acfa4642953b44ad163f3d8ffb38ec69cafed46918d1076bcfd9c30749495"
   license "FSL-1.1-MIT"
 
   bottle do
