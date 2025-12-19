6522ee910847ef49ab38073583dd75b9e95f2f0c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-19 07:04:27 +0000

    actions-up: update 1.8.0 bottle.

--- Formula/actions-up.rb
+++ Formula/actions-up.rb
@@ -7,7 +7,7 @@ class ActionsUp < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/amrkmn/tap"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "1e3bf0412dc5a8f5232d1383a5fa867a1df7f07138e68c1a08437a10e1c24b70"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "d4713f3cbf317cb1812ce6242bcb9675ee2a1c3b9b64f4daa93227c1e68948a5"
   end
 
   depends_on "node"
