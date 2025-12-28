6cd3d3f91f348b13f6845339e771c51d1b732c39
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-28 07:12:25 +0000

    rustic: update 0.10.3 bottle.

--- Formula/rustic.rb
+++ Formula/rustic.rb
@@ -8,9 +8,7 @@ class Rustic < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/amrkmn/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "85fd8c816d2cc6291af5143042351495f380da171e029ddfb593c2e6abd00d8a"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "11ef1627da14bec0d492ee0ee10b772d255822b8c54982295624a03d42234887"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ccc9c37112d9c85557fe4fd272c0d2fc46ceacffcba94ce505b4b49a70c31b19"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "c61ff25678a3ffbb1ec7e3aeb879bc75ceba7ce9f4f2390a5d554782b0d75d23"
   end
 
   depends_on "rust" => :build
