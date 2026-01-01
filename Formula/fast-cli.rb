90ce618554cfbe441a63c4ab53edfe16750f00d2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-01 21:56:51 +0000

    fast-cli: update 5.1.0 bottle.

--- Formula/fast-cli.rb
+++ Formula/fast-cli.rb
@@ -7,9 +7,7 @@ class FastCli < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/amrkmn/tap"
-    sha256 cellar: :any,                 arm64_tahoe:   "407f4e2d785a936f0a4724d88e2cb3dd1d3d702c11971f0682aeedcdaa789811"
-    sha256 cellar: :any,                 arm64_sequoia: "89bf0529ef0b5cf1ab3ae8cea3c9cc1f1c4bca52759ed4954b57027d60e3e325"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "957e8ba90fb25065e8c10a90fa10d47398a6403ed6318d9158dfa6a7054ea590"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "4a38143f10ea11e527e4678733fa2dd0d2402fb27e4c7dd1181f8b005569277a"
   end
 
   depends_on "node"
