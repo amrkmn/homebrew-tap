90a86ee8605a761546fc5c5afc2bafba36b0a55c
amrkmn <amartukiminj@gmail.com>
2026-01-30 09:02:08 +0000

    docker-credential-passage 0.1.1

---
+++ Formula/docker-credential-passage.rb
@@ -0,0 +1,18 @@
+class DockerCredentialPassage < Formula
+  desc "Docker credential helper using age encryption"
+  homepage "https://github.com/amrkmn/docker-credential-passage"
+  url "https://github.com/amrkmn/docker-credential-passage/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "b9d32646f6ac944fc0516e23a5056d643ccc7be3f5254e6b3b6331814d3b8e09"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w -X main.Version=#{version}"), "./passage/cmd"
+  end
+
+  test do
+    assert_match "docker-credential-passage/#{version}", shell_output("#{bin}/docker-credential-passage version")
+    assert_match "{}", shell_output("#{bin}/docker-credential-passage list")
+  end
+end
