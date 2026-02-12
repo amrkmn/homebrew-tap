d2a510a5c8c6426345ddf7c62cdefdf1020b3b0a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-12 07:40:29 +0000

    opencode 1.1.60

--- Formula/opencode.rb
+++ Formula/opencode.rb
@@ -1,8 +1,8 @@
 class Opencode < Formula
   desc "AI coding agent, built for the terminal"
   homepage "https://opencode.ai/"
-  url "https://github.com/anomalyco/opencode/archive/refs/tags/v1.1.53.tar.gz"
-  sha256 "50690119a9c84ad3b3741c13c8d31eb00ac41fd737e9cbd72baf2636b7249e3a"
+  url "https://github.com/anomalyco/opencode/archive/refs/tags/v1.1.60.tar.gz"
+  sha256 "cbda55ed21bcb64045b3ac9c645ad365c2f18108f57a3ee82977df7b636ac397"
   license "MIT"
   head "https://github.com/anomalyco/opencode.git", branch: "dev"
 
