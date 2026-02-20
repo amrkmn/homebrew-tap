13dd711a5b103781b65402a838dcb7e327da7aea
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-20 03:27:10 +0000

    opencode: update 1.2.9 bottle.

--- Formula/opencode.rb
+++ Formula/opencode.rb
@@ -15,7 +15,7 @@ class Opencode < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/amrkmn/tap"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "984af9e9517e1b4cce935cc88ccabb7671baa5bb82305df515db01928efbe111"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "09ceb29b3e74e60c0fef5e5c84b0e088292596fa3b9f229c5c05350994181b5a"
   end
 
   depends_on "amrkmn/bun/bun" => :build
