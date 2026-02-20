aebf22f1293f9602cf53b8e847f1f6d90c732c85
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-20 03:21:52 +0000

    opencode 1.2.9
    Closes #542.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/opencode.rb
+++ Formula/opencode.rb
@@ -1,8 +1,8 @@
 class Opencode < Formula
   desc "AI coding agent, built for the terminal"
   homepage "https://opencode.ai/"
-  url "https://github.com/anomalyco/opencode/archive/refs/tags/v1.2.6.tar.gz"
-  sha256 "fff4184074c587eb92f8c50b05418cdcf02663a0d51999c42279e77b30b53bd4"
+  url "https://github.com/anomalyco/opencode/archive/refs/tags/v1.2.9.tar.gz"
+  sha256 "7f397026e1ed75add0f2fb3d9d26b9054fd0259493559ae7467c32364ce76999"
   license "MIT"
   head "https://github.com/anomalyco/opencode.git", branch: "dev"
 
