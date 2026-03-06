cb00257e79a0e0a3f258d5fcd38fb10b4cad6508
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-06 16:20:36 +0000

    croc 10.4.0

--- Formula/croc.rb
+++ Formula/croc.rb
@@ -1,8 +1,8 @@
 class Croc < Formula
   desc "Securely send things from one computer to another"
   homepage "https://schollz.com/software/croc6"
-  url "https://github.com/schollz/croc/archive/refs/tags/v10.3.1.tar.gz"
-  sha256 "098f9f7295843b09378dd49877ef189aaafac8e6255a346a5f1f9ce310538312"
+  url "https://github.com/schollz/croc/archive/refs/tags/v10.4.0.tar.gz"
+  sha256 "c4f6266c47c113ea8601d08789a84521854e46d5c14b77a4cdc1e8c774f8d31c"
   license "MIT"
   head "https://github.com/schollz/croc.git", branch: "main"
 
