b3b9576034ef8b3e408ccd18f3cf097bc59fdee9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-17 19:52:05 +0000

    crush: update 0.60.0 bottle.

--- Formula/crush.rb
+++ Formula/crush.rb
@@ -7,7 +7,7 @@ class Crush < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/amrkmn/tap"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "98a05ee95ece849a866d66bb689a9eae0bdd617ed44532042af48c81a1515217"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "3d74fd4a0b3f677e9941829b2bfa45bec9a581e47f04767c569e94d49a842c0b"
   end
 
   depends_on "node"
