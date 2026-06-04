3a589091b1c222545a2605cb5f4a4ba89b65f40a
Amar Tukimin <amartukiminj@gmail.com>
2026-06-04 13:09:57 +0800

    git-pages-cli 1.9.0 (new formula)

---
+++ Formula/git-pages-cli.rb
@@ -0,0 +1,25 @@
+class GitPagesCli < Formula
+  desc "Tool for publishing a site to a git-pages server"
+  homepage "https://codeberg.org/git-pages/git-pages-cli"
+  url "https://codeberg.org/git-pages/git-pages-cli/archive/v1.9.0.tar.gz"
+  sha256 "d5253a17ae340b8d8d9afbfb84dde9ae1b63251cd2e195de5f69e6108ba43a81"
+  license "0BSD"
+  head "https://codeberg.org/git-pages/git-pages-cli.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X main.versionOverride=#{version}
+    ]
+    system "go", "build", *std_go_args(ldflags:)
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/git-pages-cli --version")
+
+    output = shell_output("#{bin}/git-pages-cli https://example.org --challenge 2>&1")
+    assert_match "_git-pages-challenge.example.org", output
+  end
+end
