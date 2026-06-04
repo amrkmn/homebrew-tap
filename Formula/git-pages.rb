595793b13bf2dba1452eb6d554a31ad9e665bcf4
Amar Tukimin <amartukiminj@gmail.com>
2026-06-04 13:10:09 +0800

    git-pages 0.9.1 (new formula)
    Closes #787.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/git-pages.rb
@@ -0,0 +1,33 @@
+class GitPages < Formula
+  desc "Scalable static site server for Git forges"
+  homepage "https://codeberg.org/git-pages/git-pages"
+  url "https://codeberg.org/git-pages/git-pages/archive/v0.9.1.tar.gz"
+  sha256 "6d86eff26ed036ac77a136e7f4c2a31cd7f1860af97eed7bd0b2b2202114acd2"
+  license "0BSD"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    mkdir_p "data"
+    port = free_port
+
+    (testpath/"config.toml").write <<~TOML
+      [server]
+      pages = "tcp/localhost:#{port}"
+    TOML
+
+    ENV["PAGES_INSECURE"] = "1"
+
+    spawn bin/"git-pages"
+
+    sleep 2
+    system "curl", "http://127.0.0.1:#{port}", "-X", "PUT", "-d", "https://codeberg.org/git-pages/git-pages.git"
+
+    sleep 2
+    assert_equal "It works!\n", shell_output("curl http://127.0.0.1:#{port}")
+  end
+end
