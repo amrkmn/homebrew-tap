3cf401dcb8cbc36b742f80d22c1e20f8a9aa5e8d
Amar Tukimin <amartukiminj@gmail.com>
2026-06-04 13:10:18 +0800

    rclone 1.74.2 (new formula)

---
+++ Formula/rclone.rb
@@ -0,0 +1,38 @@
+class Rclone < Formula
+  desc "Rsync for cloud storage"
+  homepage "https://rclone.org/"
+  url "https://github.com/rclone/rclone/archive/refs/tags/v1.74.2.tar.gz"
+  sha256 "2373a74751cfd2034cc6b792a9a15d119087cb77975f3c9fcd7a4503c15102b0"
+  license "MIT"
+  head "https://github.com/rclone/rclone.git", branch: "master"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/rclone/rclone/fs.Version=v#{version}
+    ]
+    tags = "brew" if OS.mac?
+    system "go", "build", *std_go_args(ldflags:, tags:)
+    man1.install "rclone.1"
+    system bin/"rclone", "genautocomplete", "bash", "rclone.bash"
+    system bin/"rclone", "genautocomplete", "zsh", "_rclone"
+    system bin/"rclone", "genautocomplete", "fish", "rclone.fish"
+    bash_completion.install "rclone.bash" => "rclone"
+    zsh_completion.install "_rclone"
+    fish_completion.install "rclone.fish"
+  end
+
+  def caveats
+    <<~EOS
+      Homebrew's installation does not include the `mount` subcommand on macOS which depends on FUSE, use `nfsmount` instead.
+    EOS
+  end
+
+  test do
+    (testpath/"file1.txt").write "Test!"
+    system bin/"rclone", "copy", testpath/"file1.txt", testpath/"dist"
+    assert_match File.read(testpath/"file1.txt"), File.read(testpath/"dist/file1.txt")
+  end
+end
