5a0cfa35d4dc5d7ee693720fc24ed2ea89c9ad46
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-04 05:19:48 +0000

    forgejo-cli: add 0.5.0 bottle.

--- Formula/forgejo-cli.rb
+++ Formula/forgejo-cli.rb
@@ -6,6 +6,11 @@ class ForgejoCli < Formula
   license any_of: ["Apache-2.0", "MIT"]
   head "https://codeberg.org/forgejo-contrib/forgejo-cli.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/amrkmn/tap"
+    sha256 cellar: :any, x86_64_linux: "c8524873de7c16a21e81398a18b0015aa5550e04d4f6f2166650e6b458ded1e5"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
   depends_on "openssl@3"
