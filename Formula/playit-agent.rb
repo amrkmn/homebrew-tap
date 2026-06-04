f79f1f20502f8410781663926fa9f8efdd4f2c56
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-04 20:52:35 +0000

    playit-agent 1.0.7

--- Formula/playit-agent.rb
+++ Formula/playit-agent.rb
@@ -1,8 +1,8 @@
 class PlayitAgent < Formula
   desc "Secure tunnel client to expose local servers via Playit.gg"
   homepage "https://github.com/playit-cloud/playit-agent"
-  url "https://github.com/playit-cloud/playit-agent/archive/refs/tags/v1.0.6.tar.gz"
-  sha256 "435850d3a12bc78baefd9e89bb6d7201a4a4ef493ad78bc14ac5a5332185da40"
+  url "https://github.com/playit-cloud/playit-agent/archive/refs/tags/v1.0.7.tar.gz"
+  sha256 "d4c57d679197f2e9b11e05de041c153716659baf7d228ec5b53a45c014278282"
   license "BSD-2-Clause"
   head "https://github.com/playit-cloud/playit-agent.git", branch: "master"
 
