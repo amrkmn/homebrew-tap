17ac017a417147f8b5280a02c4014f3ae64f26d8
Amar Tukimin <amartukiminj@gmail.com>
2026-06-04 13:10:27 +0800

    regclient 0.11.5 (new formula)

---
+++ Formula/regclient.rb
@@ -0,0 +1,28 @@
+class Regclient < Formula
+  desc "Docker and OCI Registry Client in Go and tooling using those libraries"
+  homepage "https://regclient.org/"
+  url "https://github.com/regclient/regclient/archive/refs/tags/v0.11.5.tar.gz"
+  sha256 "ad802e0dcf2852c8e263146227f3430229f911f8404c2a5aa8d4022f13776d8a"
+  license "Apache-2.0"
+  head "https://github.com/regclient/regclient.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X github.com/regclient/regclient/internal/version.vcsTag=#{version}"
+    ["regbot", "regctl", "regsync"].each do |f|
+      system "go", "build", *std_go_args(ldflags:, output: bin/f), "./cmd/#{f}"
+
+      generate_completions_from_executable(bin/f, shell_parameter_format: :cobra)
+    end
+  end
+
+  test do
+    output = shell_output("#{bin}/regctl image manifest docker.io/library/alpine:latest")
+    assert_match "docker.io/library/alpine:latest", output
+
+    assert_match version.to_s, shell_output("#{bin}/regbot version")
+    assert_match version.to_s, shell_output("#{bin}/regctl version")
+    assert_match version.to_s, shell_output("#{bin}/regsync version")
+  end
+end
