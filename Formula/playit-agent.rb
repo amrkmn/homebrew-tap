d5cdce1078a5b89064e27f35776f26e6590efe83
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-07 20:09:01 +0000

    playit-agent: update 1.0.9 bottle.

--- Formula/playit-agent.rb
+++ Formula/playit-agent.rb
@@ -13,7 +13,7 @@ class PlayitAgent < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/amrkmn/tap"
-    sha256 cellar: :any, x86_64_linux: "db623bc2a119ecd95e17bf3e2f7cb7c07736f7b743a87362f8edf17c97967bff"
+    sha256 cellar: :any, x86_64_linux: "91c960ba59a03f249dab658b75a096cd1df3c7469e31f1bdef7f2f35148bc2c0"
   end
 
   depends_on "rust" => :build
