01722446711034b243e5218862af1063ade6e7e2
Amar Tukimin <amartukiminj@gmail.com>
2026-06-22 17:27:40 +0800

    surge: revision bump
    Closes #835.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/surge.rb
+++ Formula/surge.rb
@@ -4,6 +4,7 @@ class Surge < Formula
   url "https://github.com/SurgeDM/Surge/archive/refs/tags/v0.10.0.tar.gz"
   sha256 "c9954a7a055e8b3cca5b6f761b35928a01c42964d01147cb3789d45d34d3763e"
   license "MIT"
+  revision 1
   head "https://github.com/SurgeDM/Surge.git", branch: "main"
 
   bottle do
@@ -18,7 +19,7 @@ class Surge < Formula
   def install
     ldflags = %W[
       -s -w
-      -X github.com/SurgeDM/Surge/cmd.Version=v#{version}
+      -X github.com/SurgeDM/Surge/cmd.Version=#{version}
     ]
     system "go", "build", *std_go_args(ldflags:)
   end
