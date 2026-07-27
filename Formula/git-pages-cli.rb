e8c8054eebc21c3068b02f9b593ecede0d02300a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-27 15:31:06 +0000

    git-pages-cli 1.10.1
    Closes #903.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/git-pages-cli.rb
+++ Formula/git-pages-cli.rb
@@ -1,8 +1,8 @@
 class GitPagesCli < Formula
   desc "Tool for publishing a site to a git-pages server"
   homepage "https://codeberg.org/git-pages/git-pages-cli"
-  url "https://codeberg.org/git-pages/git-pages-cli/archive/v1.10.0.tar.gz"
-  sha256 "0ac04e1dd03a3d3325c6332e28f392c8e89911b297aca44bffc1439f1c60a478"
+  url "https://codeberg.org/git-pages/git-pages-cli/archive/v1.10.1.tar.gz"
+  sha256 "45b83e7eafa9fadd1b65d64ee67a9146ab44a182cce86450b347b8ec958f5eac"
   license "0BSD"
   head "https://codeberg.org/git-pages/git-pages-cli.git", branch: "main"
 
