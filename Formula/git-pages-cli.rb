1cc49ca6133734a38626c68108ce4c5802f63cd6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-27 15:36:18 +0000

    git-pages-cli: update 1.10.1 bottle.

--- Formula/git-pages-cli.rb
+++ Formula/git-pages-cli.rb
@@ -8,9 +8,9 @@ class GitPagesCli < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/amrkmn/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "f5f6c4484101831b4135a197f2e846d1ec8299c1411016d0a0f05563d11f270c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:  "6d900283fce77c20a55533e0f4966a1564fbe80792bd37d711a8e2be042d3b88"
-    sha256 cellar: :any,                 x86_64_linux: "14725d77a5400779b1d86bff1c6c89d1b720d01fc724c6451b8d8b553a509673"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "73d3fd10b353f0b2ace33706667a90d0c8548e5d7fc99b637c5a4335ec715f0a"
+    sha256 cellar: :any_skip_relocation, arm64_linux:  "5a7ad5b50365e8062b91953c2d6e28a6c8dd672b20b9d36cc7a069a8ed7c651c"
+    sha256 cellar: :any,                 x86_64_linux: "54e4c7c6fcde9f5db0ef12e1e42b83263eea0d4dc8183510f1a0953c7e0ef70a"
   end
 
   depends_on "go" => :build
