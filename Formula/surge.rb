b1e0aa754c275244877fe99f1a5ae3454615976e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-29 09:28:57 +0000

    surge 0.11.0

--- Formula/surge.rb
+++ Formula/surge.rb
@@ -1,10 +1,9 @@
 class Surge < Formula
   desc "Blazing fast TUI download manager built in Go for power users"
   homepage "https://github.com/SurgeDM/Surge"
-  url "https://github.com/SurgeDM/Surge/archive/refs/tags/v0.10.0.tar.gz"
-  sha256 "c9954a7a055e8b3cca5b6f761b35928a01c42964d01147cb3789d45d34d3763e"
+  url "https://github.com/SurgeDM/Surge/archive/refs/tags/v0.11.0.tar.gz"
+  sha256 "aa2b646f9719a50fe791b480b50c4eede471785d047020506e9ac207310e9c4f"
   license "MIT"
-  revision 1
   head "https://github.com/SurgeDM/Surge.git", branch: "main"
 
   livecheck do
