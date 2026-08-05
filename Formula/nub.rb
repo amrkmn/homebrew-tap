8155a05d968e7d4d951b990cc92ab4e8c2dd2abc
Amar Tukimin <amartukiminj@gmail.com>
2026-08-05 17:45:28 +0800

    nub 0.6.0 (new formula)

---
+++ Formula/nub.rb
@@ -0,0 +1,24 @@
+class Nub < Formula
+  desc "Fast all-in-one Node.js toolkit"
+  homepage "https://nubjs.com"
+  url "https://registry.npmjs.org/@nubjs/nub/-/nub-0.6.0.tgz"
+  sha256 "67b8700b332883bbfa856442fcb7aa6e1891cb4d474e6a4ee4f0f328323d0a46"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/nub --version")
+
+    system bin/"nub", "init", "--yes", "--no-install", "--no-git"
+    assert_path_exists testpath/"package.json", "package.json must exist"
+    assert_equal "Hello from Nub", shell_output("#{bin}/nub --no-check index.ts").chomp
+
+    assert_match "Usage: nub nubx", shell_output("#{bin}/nubx --help")
+  end
+end
