40a6f2e13ecc49c119d562117636eb2512319449
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-05 09:51:55 +0000

    nub: add 0.6.0 bottle.

--- Formula/nub.rb
+++ Formula/nub.rb
@@ -5,6 +5,13 @@ class Nub < Formula
   sha256 "67b8700b332883bbfa856442fcb7aa6e1891cb4d474e6a4ee4f0f328323d0a46"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/amrkmn/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "5866c317ead470f287e3ba1d6f90095c0f9c51469d7ab70fc8023bfe14ac5d09"
+    sha256 cellar: :any_skip_relocation, arm64_linux:  "720827fdb28fa650993033c2adfb8fed61874d7a8393c935d334552121cdc8ef"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "819ed0f92e34193df8a69df50337801052244262af95af1436394ddb7c8b77e2"
+  end
+
   depends_on "node"
 
   def install
