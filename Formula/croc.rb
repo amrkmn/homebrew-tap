9f0d438c827cc968a091147bc5e550bdbf8144e1
Amar Tukimin <amartukiminj@gmail.com>
2026-08-26 15:26:53 +0800

    croc: following homebrew's config
    Closes #1027.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/croc.rb
+++ Formula/croc.rb
@@ -1,17 +1,10 @@
 class Croc < Formula
   desc "Securely send things from one computer to another"
-  homepage "https://getcroc.com/"
+  homepage "https://github.com/schollz/croc"
   url "https://github.com/schollz/croc/archive/refs/tags/v11.3.2.tar.gz"
   sha256 "441f751cc45d0a66312a54bf5374a6f30e95992e0d71c808d5aaadb62f74cd67"
   license "MIT"
   head "https://github.com/schollz/croc.git", branch: "main"
-
-  bottle do
-    root_url "https://ghcr.io/v2/amrkmn/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "48446a2ede7a67070240a3847af801bbb72e84d5208537d3739a9375a093f487"
-    sha256 cellar: :any_skip_relocation, arm64_linux:  "7db0b2e7534a12b1533de6c827474baed494e98c5ca8eb293836b8cb0211298d"
-    sha256 cellar: :any,                 x86_64_linux: "6b054280f7023a97511015bf40b1731d16ff5184b480c6601f82b75b36151871"
-  end
 
   depends_on "go" => :build
 
@@ -26,24 +19,14 @@ class Croc < Formula
     ports = [free_port, free_port]
 
     require "pty"
-    require "socket"
     pid = PTY.spawn(bin/"croc", "relay", "--ports", ports.join(",")).last
-    relay_ready = false
-    60.times do
-      TCPSocket.new("localhost", ports.first).close
-      relay_ready = true
-      break
-    rescue SystemCallError
-      sleep 0.5
-    end
-    flunk "croc relay did not start" unless relay_ready
+    sleep 3
 
-    pid_send = PTY.spawn(bin/"croc", "--transport=relay",
-                                     "--relay=localhost:#{ports.first}", "send",
+    pid_send = PTY.spawn(bin/"croc", "--relay=localhost:#{ports.first}", "send",
                                      "--no-local", "--text=mytext", "--transfers=1").last
     sleep 3
 
-    output = shell_output("#{bin}/croc --transport=relay --relay localhost:#{ports.first} --overwrite --yes")
+    output = shell_output("#{bin}/croc --relay localhost:#{ports.first} --overwrite --yes")
     assert_match "mytext", output
   ensure
     Process.kill("TERM", pid_send)
