9da50c033d108fe14b41c8ec6726c85383272efe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-30 06:38:09 +0000

    nub: update 0.8.1 bottle.

--- Formula/nub.rb
+++ Formula/nub.rb
@@ -12,9 +12,9 @@ class Nub < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/amrkmn/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "c4653c49b2e2c2bbe4c2af364b160fea33df8401c8840510fb351fef2512fca3"
-    sha256 cellar: :any_skip_relocation, arm64_linux:  "79fc2164cb55b06314c28931aa01595b4e508dcea3b6fa5bf045a323c4d4eeb6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "3320b0a7a2d06659c7be425df9fa8b0a459b8eb66255d4a099d5409390284086"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "66b62e7de1d7d993bd11f8ea4a94f51ec1fda8116a7187bb56dda8c5ed0bb0a2"
+    sha256 cellar: :any,                 arm64_linux:  "9f68756491fca46980a4738f249b73ae8d9467e52922f36d9a5c9bba152a9b68"
+    sha256 cellar: :any,                 x86_64_linux: "c5f51d1094af92709e1909838ddc8b98dd30324e20902a95a13a6c2dbf561ad5"
   end
 
   depends_on "cmake" => :build
