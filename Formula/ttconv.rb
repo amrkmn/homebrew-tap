5593c22e550affa025d60ed4998e28f3d3848019
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-31 21:11:17 +0000

    ttconv: update 1.2.3 bottle.

--- Formula/ttconv.rb
+++ Formula/ttconv.rb
@@ -9,7 +9,9 @@ class Ttconv < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/amrkmn/tap"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "94c0e5a512fa056f44c00ed136c70f5e340412789744bbd51b749780b9f28923"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "6d7b1a77208d0c08c4d74e80bcca6db3fb019b13302b13cb7ee03e71f017e9bf"
+    sha256 cellar: :any_skip_relocation, arm64_linux:  "cc5e895e572cc695ae604175ef0ff21e96d5b5b949d3c39e4ce301a1d6174d0a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "7656f33bc148f0a0e292baf9c2d5a50f0947e847cb03082d78057ac9a1621635"
   end
 
   depends_on "python@3.14"
